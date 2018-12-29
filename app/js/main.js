ab94b192f876c91e8e1f2622d0b2c6f97b93ea66
Guido La Rosa <guido.glarosa@gmail.com>
2018-12-29 12:28:13 -0300

    Site near completed

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,8 @@ $(document).ready(function()
 {
     // Scroll to animation for the navbar
 
-    $('navbar a').click(function()
+    $('.navbar a').click(function()
     {
-        console.log(this)
         let target = $(this)[0].attributes[1].nodeValue;
         console.log(target)
         $([document.documentElement, document.body]).animate({
@@ -14,4 +13,10 @@ $(document).ready(function()
         }, 1000);
     })
     
+    // Show navbar when clicking the menu container
+    
+    $('.display-menu-container').click(function()
+    {   
+        $('.navbar-container').toggleClass('navbar-show');
+    })
 })
--- dist/main.js
+++ dist/main.js
@@ -4,13 +4,18 @@
 $(document).ready(function () {
     // Scroll to animation for the navbar
 
-    $('navbar a').click(function () {
-        console.log(this);
+    $('.navbar a').click(function () {
         let target = $(this)[0].attributes[1].nodeValue;
         console.log(target);
         $([document.documentElement, document.body]).animate({
             scrollTop: $("." + target).offset().top
         }, 1000);
+    });
+
+    // Show navbar when clicking the menu container
+
+    $('.display-menu-container').click(function () {
+        $('.navbar-container').toggleClass('navbar-show');
     });
 });
 },{}]},{},[1])
